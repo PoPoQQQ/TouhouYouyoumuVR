495eb212bc9d76f27f99fae1a2e16b3c0890fc1d
leenldk <3348263546@qq.com>
2020-12-05 00:00:21 +0800

    add start menu

---
+++ Assets/Scripts/UI/ChangeColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColor : MonoBehaviour
+{
+    public bool change = false;
+    // Start is called before the first frame update
+
+    Image image;
+
+    void Start()
+    {
+        image = gameObject.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(change)
+        {
+            image.color = Color.red;
+        }
+        else
+        {
+            image.color = Color.white;
+        } 
+    }
+}
---
+++ Assets/Scripts/UI/UISelectBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISelectBehavior : MonoBehaviour
+{
+    public GameObject unselected;
+    public GameObject selected;
+
+    public bool isSelecting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        unselected.SetActive(true);
+        selected.SetActive(false);
+        isSelecting = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isSelecting)
+        {
+            unselected.SetActive(false);
+            selected.SetActive(true);
+        }
+        else
+        {
+            unselected.SetActive(true);
+            selected.SetActive(false);
+        }
+    }
+}
